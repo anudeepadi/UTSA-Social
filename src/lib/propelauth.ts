a4a35fbeb364602e30fabfc593f4df6c88e9d604
anudeepadi <anudeep.adiraju@gmail.com>
2024-10-27 12:08:35 -0500

    s

--- src/lib/propelauth.ts
+++
@@ -1,7 +0,0 @@
-import { initBaseAuth } from '@propelauth/react';
-
-initBaseAuth({
-  authUrl: process.env.NEXT_PUBLIC_PROPELAUTH_URL as string,
-  // Optional configuration
-  enableBackgroundTokenRefresh: true,
-});
